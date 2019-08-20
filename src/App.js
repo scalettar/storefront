325e577da549f4cf6cda6f02468e5fb49aa43368
Daniel Scalettar <scalettar+github@gmail.com>
2019-08-20 16:31:39 -0700

    Create signinpage, sign-in, and form-input components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
+import SigninPage from './pages/signinpage/signinpage.component';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route page='/signin' component={SigninPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/signinpage/signinpage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './signinpage.styles.scss';
+
+const SigninPage = () => (
+    <div className='signinpage'><SignIn /></div>
+)
+
+export default SigninPage;

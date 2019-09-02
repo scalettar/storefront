c1674410d6152cda7b74440a5b7928b0213203c6
Daniel Scalettar <scalettar+github@gmail.com>
2019-09-01 19:58:22 -0700

    Add cart utils function to group items with quantity parameter

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,5 +1,9 @@
 import CartActionTypes from './cart.types';
 
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+})
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
 });

95217479f550cc121ef7f12c1758fdf32a8ecc5e
Daniel Scalettar <scalettar+github@gmail.com>
2019-09-17 20:20:20 -0700

    Add stripe payment integration to checkoutpage component

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_7cIMjYYugYsieL9AZNtCWlLu00xRY6GA9X';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='Regal Clothing Co.'
+            billingAddress
+            shippingAddress
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;

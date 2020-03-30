deea8064b9d45a2a7fac03b731f0d6072f2bb8bf
Daniel Scalettar <scalettar+github@gmail.com>
2020-03-30 11:14:00 -0700

    Updates

---
+++ precache-manifest.97825d192e53201135196a14b4367488.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd73505e424f5e95cc062bfecc129c1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aa4559b4a155ed3fb3c",
+    "url": "/static/css/main.f9017c76.chunk.css"
+  },
+  {
+    "revision": "c8f1cfa9ec68dc2efb27",
+    "url": "/static/js/2.f1ecb821.chunk.js"
+  },
+  {
+    "revision": "fbe02c2169f8f7c792e446ab8fec70b4",
+    "url": "/static/js/2.f1ecb821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa4559b4a155ed3fb3c",
+    "url": "/static/js/main.35fb7e8f.chunk.js"
+  },
+  {
+    "revision": "cb98670ee08b55696784",
+    "url": "/static/js/runtime-main.603c1494.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
